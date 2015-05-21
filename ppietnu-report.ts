2f1f79c13d22f78c1f9c72caf8e743494156c72f
betamint <betamint@gmail.com>
2015-05-22 02:02:01 +0800

    subsubsection style corrected

--- ppietnu-report.ts
+++ ppietnu-report.ts
@@ -102,7 +102,7 @@
 
   <assign|subsubsection-title|<macro|name|<style-with|src-compact|none|<sectional-normal|<vspace*|0.5fs><with|font|simhei|font-size|1.2fs|<arg|name>><vspace|0.5fs>>>>>
 
-  <assign|subsubsection-numbered-title|<macro|title|<style-with|src-compact|none|<subsubsection-title|<with|font|roman|<the-subsection>><subsection-sep><arg|title>>>>>
+  <assign|subsubsection-numbered-title|<macro|title|<style-with|src-compact|none|<subsubsection-title|<with|font|roman|<the-subsubsection>><subsubsection-sep><arg|title>>>>>
 
   <assign|chapter-clean|<macro|<reset-section><reset-equation><reset-figure><reset-table>>>
 
